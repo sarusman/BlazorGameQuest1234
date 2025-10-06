8cd08428831ba85c7484c764f44bd8a9669d792b
Sarusman <sarusmandu92@gmail.com>
2025-10-06 09:37:13 +0200

    Add Test Directory

---
+++ BlazorGame.Tests/AdminTests/AdminServiceTests.cs
@@ -0,0 +1,1 @@
+namespace BlazorGame.Tests;
---
+++ BlazorGame.Tests/GameTests/GameLogicTests.cs
@@ -0,0 +1,21 @@
+namespace BlazorGame.Tests;
+
+
+public class GameLogicTests
+{
+    [Fact]
+    public void Dungeon_ShouldContainBetween1And5Rooms()
+    {
+    }
+
+    [Fact]
+    public void PlayerAction_Combat_ShouldAffectScore()
+    {
+    }
+
+    [Fact]
+    public void PlayerAction_Flee_ShouldReducePoints()
+    {
+    }
+}
+
