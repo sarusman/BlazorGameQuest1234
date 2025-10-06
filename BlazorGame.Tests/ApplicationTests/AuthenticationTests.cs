a5e41ce9de0dc5756e279b2422dfe43c2ee0dbab
Saru <sarusman.satkunarajah1@gmail.com>
2025-10-06 18:41:08 +0200

    Add Tests Directory

---
+++ BlazorGame.Tests/ApplicationTests/AuthenticationTests.cs
@@ -0,0 +1,20 @@
+namespace BlazorGame.Tests;
+
+public class AuthenticationTests
+{
+    [Fact]
+    public void Login_ShouldReturnTrue_ForValidCredentials()
+    {
+        // Arrange / Act / Assert
+    }
+
+    [Fact]
+    public void Login_ShouldFail_ForInvalidCredentials()
+    {
+    }
+
+    [Fact]
+    public void UserRole_ShouldBeAdmin_WhenAdminLoggedIn()
+    {
+    }
+}
