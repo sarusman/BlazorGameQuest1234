711cc5f0f1ae7f9f23ff979486cd8bfc93fc6505
KANLINSOU Godwin <110693163+GodWindows@users.noreply.github.com>
2025-10-13 14:20:29 +0200

    Add comments to UnitTest1 for clarity
    Added comments to clarify test intentions.

--- BlazorGame.Tests/UnitTest1.cs
+++ BlazorGame.Tests/UnitTest1.cs
@@ -2,6 +2,15 @@ namespace BlazorGame.Tests;
 
 public class UnitTest1
 {
+
+    //ON veut rester l'effective mise à jour des historique à la fin d'une partie
+
+    //On veut tester la mise à jour des données quand un joueur gagne une partie
+
+    // on veut tester la mise à jour des données quand le joueur perd une partie
+
+    // On veut tester la modification du joueur quand l'admin change ses infos
+    
     [Fact]
     public void Test1()
     {
